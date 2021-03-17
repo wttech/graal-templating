1779fd709aa3173ff19156cff85c8e497a83346a
Tomasz Krug <tomasz.krug@gmail.com>
2021-03-17 10:37:23 +0100

    Axion workaround part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,22 @@ configure<VersionConfig> {
     checks.aheadOfRemote = false
     checks.isSnapshotDependencies = false
 
+    val antora = "templating/src/main/docs/antora/antora.yml"
+    val bridgePackageJson = "templating/src/main/js/bridge/package.json"
+    val bridgeReactPackageJson = "templating/src/main/js/bridge-react/package.json"
+
     hooks.pre("fileUpdate", mutableMapOf(
-            "files" to listOf("templating/src/main/docs/antora/antora.yml"),
-            "pattern" to KotlinClosure2<String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext, String>({ _, _ -> "version: '(.*)'" }),
+            "files" to listOf(antora),
+            "pattern" to KotlinClosure2<String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext, String>({ _, c ->
+                c.addCommitPattern(antora)
+                "version: '(.*)'" }),
             "replacement" to KotlinClosure2<String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext, String>({ v, _ -> "version: '$v'" })
     ))
     hooks.pre("fileUpdate", mutableMapOf(
-            "files" to listOf("templating/src/main/js/bridge/package.json", "templating/src/main/js/bridge-react/package.json"),
+            "files" to listOf(bridgePackageJson, bridgeReactPackageJson),
             "pattern" to KotlinClosure2<String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext, String>({ _, c ->
-                c.addCommitPattern("templating/src/main/js/bridge/package.json")
-                c.addCommitPattern("templating/src/main/js/bridge-react/package.json")
+                c.addCommitPattern(bridgePackageJson)
+                c.addCommitPattern(bridgeReactPackageJson)
                 "\"version\": \"(.*)\"" }),
             "replacement" to KotlinClosure2<String, pl.allegro.tech.build.axion.release.domain.hooks.HookContext, String>({ v, _ -> "\"version\": \"$v\"" })
     ))
